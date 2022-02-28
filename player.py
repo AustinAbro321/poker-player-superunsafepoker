5e3f70ab2eab59f09ac4d62597842ff5bb939be4
UNSAFe POKEr <jay+unsafe+poker@bazuzi.com>
2022-02-28 19:59:34 +0000

    Update player.py

--- player.py
+++ player.py
@@ -3,7 +3,8 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        return 0
+        return 'check'
+        # return 0
 
     def showdown(self, game_state):
         pass
