0618289dd82d6ee40a09ba37ce275ab9d5032723
UNSAFe POKEr <jay+unsafe+poker@bazuzi.com>
2022-03-01 19:54:26 +0000

    Update player.py

--- player.py
+++ player.py
@@ -3,6 +3,7 @@ class Player:
     VERSION = "UNSAFe v1"
 
     def betRequest(self, game_state):
+        return 0
         current_buy_in = game_state["current_buy_in"]
         players = game_state["players"]
         in_action = game_state["in_action"]
