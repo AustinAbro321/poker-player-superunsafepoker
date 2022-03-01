aeffa062d4b9e13f7f97c64d58bc791acd307451
UNSAFe POKEr <jay+unsafe+poker@bazuzi.com>
2022-03-01 19:59:52 +0000

    Update player.py

--- player.py
+++ player.py
@@ -3,12 +3,13 @@ class Player:
     VERSION = "UNSAFe v1"
 
     def betRequest(self, game_state):
-
-        current_buy_in = game_state.get("current_buy_in",100)
-        players = game_state["players"]
-        in_action = game_state["in_action"]
-
-        return current_buy_in - players[in_action]["bet"]
+        try:
+            current_buy_in = game_state.get("current_buy_in",100)
+            players = game_state["players"]
+            in_action = game_state["in_action"]
+            return current_buy_in - players[in_action]["bet"]
+        except:
+            return 0
 
     def showdown(self, game_state):
         pass
