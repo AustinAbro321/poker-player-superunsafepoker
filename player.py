0b6e5e182288744fc3d49a698a6e25b020240904
UNSAFe POKEr <jay+unsafe+poker@bazuzi.com>
2022-03-03 19:17:36 +0000

    catch and log

--- player.py
+++ player.py
@@ -5,13 +5,17 @@ class Player:
     VERSION = "UNSAFe v2"
 
     def betRequest(self, game_state):
-        current_buy_in = game_state.get("current_buy_in", 100)
-        players = game_state["players"]
-        in_action = game_state["in_action"]
-        hole_cards = players[in_action]["hole_cards"]
-        community_cards = game_state["community_cards"] or []
-        bet_adjustment = 10 if is_pair(hole_cards + community_cards) else 0
-        return current_buy_in - players[in_action]["bet"] + bet_adjustment
+        try:
+            current_buy_in = game_state.get("current_buy_in", 100)
+            players = game_state["players"]
+            in_action = game_state["in_action"]
+            hole_cards = players[in_action]["hole_cards"]
+            community_cards = game_state["community_cards"] or []
+            bet_adjustment = 10 if is_pair(hole_cards + community_cards) else 0
+            return current_buy_in - players[in_action]["bet"] + bet_adjustment
+        except Exception as e:
+            print(e, file=sys.stderr)
+            return 0
 
     def showdown(self, game_state):
         pass
